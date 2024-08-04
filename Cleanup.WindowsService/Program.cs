fd5461dc23f5eaf09337590ec7f47e4c97fbbd35
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-08-04 19:27:16 +0300

    add Installer...

--- Cleanup.WindowsService/Program.cs
+++ Cleanup.WindowsService/Program.cs
@@ -1,6 +1,7 @@
 using Cleanup.WindowsService;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
+using CliWrap;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddWindowsService(options => { options.ServiceName = "Cleanup Windows Service"; });
@@ -13,6 +14,40 @@ builder.Services.AddHostedService<WindowsBackgroundService>();
 
 builder.Logging.AddConfiguration(
     builder.Configuration.GetSection("Logging"));
+
+const string ServiceName = "Cleanup Windows Service";
+
+if (args is { Length: 1 })
+{
+    try
+    {
+        string executablePath =
+            Path.Combine(AppContext.BaseDirectory, "Cleanup.WindowsService.exe");
+
+        if (args[0] is "/Install")
+        {
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "create", ServiceName, $"binPath={executablePath}", "start=auto" })
+                .ExecuteAsync();
+        }
+        else if (args[0] is "/Uninstall")
+        {
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "stop", ServiceName })
+                .ExecuteAsync();
+
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "delete", ServiceName })
+                .ExecuteAsync();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+
+    return;
+}
 
 IHost host = builder.Build();
 host.Run();
